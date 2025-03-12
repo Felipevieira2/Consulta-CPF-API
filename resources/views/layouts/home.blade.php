f76036300f58e64eafa04de49cf0a12a6b6e20d8
Felipe Vieira <felipe.vieira@fcati.com.br>
2025-03-12 14:14:01 -0300

    alterçõess

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Consulta CPF API') }}</title>
+    <title>Consulta CPF API</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
