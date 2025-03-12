9d551e7305440d81fd2300f6c0b8537412ec5449
Felipe Vieira <felipe.vieira@fcati.com.br>
2025-03-12 12:17:49 -0300

    alterações

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,10 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            RoleSeeder::class,
             UserSeeder::class,
             PlanSeeder::class,
+
             // Outros seeders...
         ]);
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,25 +10,29 @@ class UserSeeder extends Seeder
     public function run()
     {
         // Criar usuário admin
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Administrador',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('admin123'),
+            'email' => 'admin@fcati.com.br',
+            'password' => bcrypt('12121212'),
             'role' => 'admin',
             'email_verified_at' => now(),
         ]);
 
+        $user->assignRole('admin');
+
         // Criar usuário normal
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Usuário Normal',
-            'email' => 'user@example.com',
-            'password' => bcrypt('user123'),
+            'email' => 'user@fcati.com.br',
+            'password' => bcrypt('12121212'),
             'role' => 'user',
             'email_verified_at' => now(),
         ]);
 
-        // Criar mais alguns usuários aleatórios para teste
-        User::factory()->count(5)->create(); // 5 usuários normais
-        User::factory()->admin()->count(2)->create(); // 2 admins adicionais
+        $user->assignRole('user');
+
+        // // Criar mais alguns usuários aleatórios para teste
+        // User::factory()->count(5)->create(); // 5 usuários normais
+        // User::factory()->admin()->count(2)->create(); // 2 admins adicionais
     }
 } 
